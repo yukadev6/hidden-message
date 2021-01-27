c289cfb317db4059fd5e9526149dcff08f90b66c
Yuka <yuka.couweleers@gmail.com>
2021-01-26 21:28:42 -0500

    updating file

--- blackjack.py
+++ blackjack.py
@@ -33,4 +33,32 @@ class Deck:
     def deal(self):
         return self.deck.pop()
 
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,new_card):
+        self.cards.append(new_card)
+        self.value += values[card.rank]
+
+    def adjust_for_ace(self):
+        if self.value > 10
+            'Ace' == 1
+        else:
+            'Ace' == 11
+
+class Chips:
+    
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        return self.total += self.bet
+    
+    def lose_bet(self):
+        return self.total -= self.bet
+
         
